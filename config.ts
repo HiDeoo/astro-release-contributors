831df6d92397ab3fd60fdb72469ee8da9ea12d68
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-01-29 14:52:38 +0100

    feat: update for astro 5.18

--- config.ts
+++ config.ts
@@ -7,13 +7,13 @@ import type { Repo } from './src/libs/github'
  * To easily grab the date, you can use the following approach:
  *
  *   1. Open on GitHub the release commit of the last minor Astro release, e.g.
- *      https://github.com/withastro/astro/commit/e8786795fc462374d44917dd8eb6effcd6939257 for Astro 5.16.0.
+ *      https://github.com/withastro/astro/commit/08d38c6ca489142e923383a8b19ad1038313d30d for Astro 5.17.0.
  *   2. Append `.patch` at the end of the URL, e.g.
- *      https://github.com/withastro/astro/commit/e8786795fc462374d44917dd8eb6effcd6939257.patch
+ *      https://github.com/withastro/astro/commit/08d38c6ca489142e923383a8b19ad1038313d30d.patch
  *   3. Copy the `Date` from the third line of the patch file, e.g.
- *      `Date: Thu, 20 Nov 2025 05:16:08 -0800` and paste it below.
+ *      `Date: Thu, 29 Jan 2026 03:07:19 -0800` and paste it below.
  */
-export const SinceDate = new Date('Thu, 20 Nov 2025 05:16:08 -0800')
+export const SinceDate = new Date('Thu, 29 Jan 2026 03:07:19 -0800')
 
 /**
  * The list of repositories to find contributors in using the `owner/repo` format.
